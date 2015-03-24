7ca91ab84c86bf6ac8e818d6fcd5dcc0347d4fc9
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-03-23 20:24:38 -0400

    Add about and contact pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,4 +11,13 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "shoud get contact" do
+    get :contact
+    assert_response :success
+  end
 end
