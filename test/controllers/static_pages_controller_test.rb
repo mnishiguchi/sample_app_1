c40bcb4c48c4aaeccdee59afb49a25fdd9c94c6d
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-03-23 20:52:27 -0400

    Improve page title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,20 +4,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select 'title', "Home | Sample App 1"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select 'title', "Help | Sample App 1"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select 'title', "About | Sample App 1"
   end
 
   test "shoud get contact" do
     get :contact
     assert_response :success
+    assert_select 'title', "Contact | Sample App 1"
   end
 end
