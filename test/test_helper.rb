c27c78980fdb9ba33035296ae6d72f2b8f12f8ce
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-03-25 18:52:58 -0400

    Configure the tests to show red and green.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+# Get the default Rails tests to show red and green at the appropriate times
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
