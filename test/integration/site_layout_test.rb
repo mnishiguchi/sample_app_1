b69df24feb9d1dc84f89284253ac5390b8f6cd0c
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-03-28 17:11:20 -0400

    Add named routes to hyperlinks; Add site layout integration test

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  # 1.Get the root path (Home page)
+  # 2.Verify that the right page template is rendered
+  # 3.Check for the correct links to the Home, Help, About, and Contact pages
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', contact_path
+  end
+
+end
