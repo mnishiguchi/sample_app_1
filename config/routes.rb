f8e94df7b1affba90be49eb1fb7f62fb0f2e5067
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-03-31 20:53:47 -0400

    Add user controller; Add signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
+  root 'static_pages#home'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'help'    => 'static_pages#help'
-
-  root 'static_pages#home'
+  get 'signup'  => 'users#new'
 end
