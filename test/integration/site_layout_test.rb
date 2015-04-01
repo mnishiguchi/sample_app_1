23e96ff47cfe0985d4c6383df70bc7c296d7f264
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-03-31 21:17:45 -0400

    Include application helper in test helper in order to make full title method available in tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,6 +9,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # 2.Verify that the right page template is rendered
   # 3.Check for the correct links to the Home, Help, About, and Contact pages
   test "layout links" do
+    # Home page
     get root_path
     assert_template 'static_pages/home'
 
@@ -17,6 +18,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', help_path
     assert_select 'a[href=?]', contact_path
     assert_select 'a[href=?]', signup_path
-  end
 
+    # Sign up page
+    get signup_path
+    assert_select 'title', full_title("Sign up")
+  end
 end
