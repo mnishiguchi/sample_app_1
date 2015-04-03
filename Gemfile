b9cd827dee2330e46bb8bc4244eb6a2cc22a14d0
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-04-03 19:33:57 -0400

    Add secure password with minimum length validation, along with test

--- Gemfile
+++ Gemfile
@@ -2,20 +2,20 @@ source 'https://rubygems.org'
 # ruby '2.2.1'
 
 gem 'rails',                   '4.2.0'
-gem 'bcrypt',                  '3.1.7'
+gem 'bcrypt',                  '3.1.7'  # for password digest
 gem 'faker',                   '1.4.2'
 gem 'carrierwave',             '0.10.0'
 gem 'mini_magick',             '3.8.0'
 gem 'fog',                     '1.23.0'
 gem 'will_paginate',           '3.0.7'
 gem 'bootstrap-will_paginate', '0.0.10'
-gem 'bootstrap-sass',          '3.2.0.0'
+gem 'bootstrap-sass',          '3.2.0.0'  # converts Less to Sass
 gem 'sass-rails',              '5.0.1'
 gem 'uglifier',                '2.5.3'
 gem 'coffee-rails',            '4.1.0'
 gem 'jquery-rails',            '4.0.3'
-gem 'haml-rails',              '0.9'
-gem 'redcarpet',               '3.2.2'
+gem 'haml-rails',              '0.9'    # for HAML
+gem 'redcarpet',               '3.2.2'  # for HAML
 gem 'turbolinks',              '2.3.0'
 gem 'jbuilder',                '2.2.3'
 gem 'sdoc',                    '0.4.0', group: :doc
@@ -28,8 +28,11 @@ group :development, :test do
 end
 
 group :test do
+  # get the default Rails tests to show red and green
   gem 'minitest-reporters', '1.0.5'
+  # filter the backtrace to eliminate unwanted lines
   gem 'mini_backtrace',     '0.1.3'
+  # automate the running of the tests
   gem 'guard-minitest',     '2.3.1'
 end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,12 +4,15 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     # create a user in memory.
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
   
   test "should be valid" do
     assert @user.valid?
   end
+
+  # User Name
 
   test "name should be present" do
     blank_name = "     "
@@ -17,15 +20,17 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "email should be present" do
-    blank_email = "    "
-    @user.email = blank_email
-    assert_not @user.valid?
-  end
-
   test "name should not be too long" do
     invalid_name = "a" * 51
     @user.name = invalid_name
+    assert_not @user.valid?
+  end
+
+  # User Email
+
+  test "email should be present" do
+    blank_email = "    "
+    @user.email = blank_email
     assert_not @user.valid?
   end
 
@@ -38,8 +43,11 @@ class UserTest < ActiveSupport::TestCase
   test "email validation should accept valid addresses" do
     # Email format validation is tricky and error-prone
     # make sure valid addressed are accepted while invalid ones are rejected
-    valid_addresses = %w(user@example.com USER@foo.COM
-                        A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn)
+    valid_addresses = %w(user@example.com
+                        USER@foo.COM
+                        A_US-ER@foo.bar.org
+                        first.last@foo.jp
+                        alice+bob@baz.cn)
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       # display specific address that failed in case of error
@@ -48,8 +56,12 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should reject invalid addresses" do
-    invalid_addresses = %w(user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com)
+    invalid_addresses = %w(user@example,com
+                          user_at_foo.org
+                          user.name@example.
+                          foo@bar_baz.com
+                          foo@bar+baz.com
+                          foo@bar..com)
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       # display specific address that failed in case of error
@@ -62,5 +74,21 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user = @user.dup        # try to sign up with the same address
     duplicate_user.email = @user.email.upcase  # check for case insensitivity
     assert_not duplicate_user.valid?  # should be rejected
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save  # save @user to database
+    # reload @user from database and verify email was converted to lowercase
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  # Password
+
+  test "password should have a minimum length" do
+    invalid_password = "a" * 5
+    @user.password = @user.password_confirmation = invalid_password
+    assert_not @user.valid?
   end
 end
