e1efeea4a62987ef96082edcafd08be360cbc6c3
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-04-03 14:38:03 -0400

    Add email format validation along with test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -34,4 +34,26 @@ class UserTest < ActiveSupport::TestCase
     @user.email = invalid_email
     assert_not @user.valid?
   end
+
+  test "email validation should accept valid addresses" do
+    # Email format validation is tricky and error-prone
+    # make sure valid addressed are accepted while invalid ones are rejected
+    valid_addresses = %w(user@example.com USER@foo.COM
+                        A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn)
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      # display specific address that failed in case of error
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w(user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com)
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      # display specific address that failed in case of error
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
 end
