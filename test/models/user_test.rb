9b2a21b8865635dbbe13f5b557b71b1539789bfb
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-04-03 07:10:57 -0400

    Add presence validation to user model along with test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    # create a user in memory.
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    blank_name = "     "
+    @user.name = blank_name
+    assert_not @user.valid?
+  end
 end
