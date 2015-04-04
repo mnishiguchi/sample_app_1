f14ed62b79fc4b3f3a31d47295030e6457c179cb
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-04-03 20:17:30 -0400

    Add masa's photo on the home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-                    
-  validates :password, length: { minimum: 6 }
 
-  has_secure_password
+  has_secure_password  # incl presence and matching validations
+  validates :password, length: { mininum: 6 }
 end
