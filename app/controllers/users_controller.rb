0760b164c56c46990aff98426482692300083eb2
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-04-04 18:45:07 -0400

    Add sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # handle a successful save
+    else
+      render 'new'  # re-render 'new' template (signup form)
+    end
+  end
+
+  private
+
+    def user_params
+      # Must submit user and only specified attributes are permitted
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
