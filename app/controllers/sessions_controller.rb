8fd7a4327952b7e0989462d1bc5c7bd49e7e0282
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-04-05 13:23:30 -0400

    Add log_in method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,9 @@
-=begin
-Unlike the Users resource, which uses a database back-end (via the User model)
-to persist data, the Sessions resource will use cookies, and much of the work 
-involved in login comes from building this cookie-based authentication machinery
-=end
+
+# Unlike the Users resource, which uses a database back-end (via the User model)
+# to persist data, the Sessions resource will use cookies, and much of the work 
+# involved in login comes from building this cookie-based authentication machinery
+# Instance variable is not required here because we won't render any template.
+
 class SessionsController < ApplicationController
   
   def new
@@ -11,7 +12,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      # log the user in and redirect the user's show page
+      log_in user
+      redirect_to user  # redirect to the user's profile page
     else
       # create an error message
       flash.now[:danger] = 'Invalid email/password combination'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+
+  # We can treat Rails' built-in session method as if it were a hash.
+  # The assigned value will be encrypted and placed as a temporary cookie on the
+  # user’s browser.
+  # The temporary cookie allows us to retrieve the id on subsequent pages using
+  # session[:user_id].
+
+  # log in the given user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 end
