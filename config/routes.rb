23bc0ad02756ee855de62861ecf887f0ed70370d
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-04-05 10:35:36 -0400

    Add sessions controller; Configure routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  
-  root 'static_pages#home'
-  get  'about'   => 'static_pages#about'
-  get  'contact' => 'static_pages#contact'
-  get  'help'    => 'static_pages#help'
-  get  'signup'  => 'users#new'
+
+  root                'static_pages#home'
+
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'help'    => 'static_pages#help'
+
+  get    'signup'  => 'users#new'
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   resources :users
 end
