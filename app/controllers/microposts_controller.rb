926dd26a15708f70732c42f61b25bf3604977cb8
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-05-01 15:18:09 -0400

    Add microposts creation form; Refactor error-message partial

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,12 +3,25 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
-
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
-
   end
 
-      # NOTE: The logged_in_user filter is defined in ApplicationController.
+  #----------------------------------------------------------------------------
+  private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+
+    end
+
+    # NOTE: The logged_in_user filter is defined in ApplicationController.
 end
