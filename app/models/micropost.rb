3d880a7d7a174c148712c66833537481c56e427c
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-05-02 11:55:07 -0400

    Add image validations, both front- and back-ends

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,4 +7,14 @@ class Micropost < ActiveRecord::Base
 
   # For image upload using carrierwave.
   mount_uploader :picture, PictureUploader
+  validate  :picture_size
+
+  private
+
+    # Validates the size of an uploaded picture.
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")  # Custom error message.
+      end
+    end
 end
