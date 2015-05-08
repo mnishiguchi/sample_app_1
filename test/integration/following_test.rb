2e0963b4fe501ca20163ffff3745551d8722fb7d
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-05-08 06:11:50 -0400

    Add test for follow/unfollow button

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -4,6 +4,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:masa)
+    @other = users(:archer)
     log_in_as(@user)
   end
 
@@ -30,6 +31,33 @@ class FollowingTest < ActionDispatch::IntegrationTest
     # Each item should have a link to his/her profile page.
     @user.followers.each do |user|
       assert_select "a[href=?]", user_path(user)
+    end
+  end
+  test "should follow a user the standard way" do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, followed_id: @other.id
+    end
+  end
+
+  test "should follow a user with Ajax" do
+    assert_difference '@user.following.count', 1 do
+      xhr :post, relationships_path, followed_id: @other.id
+    end
+  end
+
+  test "should unfollow a user the standard way" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship)
+    end
+  end
+
+  test "should unfollow a user with Ajax" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference '@user.following.count', -1 do
+      xhr :delete, relationship_path(relationship)
     end
   end
 end
