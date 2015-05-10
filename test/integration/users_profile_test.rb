8658234ed558da92bb5bc65dae7d8577b445c67b
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-05-10 10:38:44 -0400

    Add test for following stats

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -10,13 +10,21 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
   test "profile display" do
     get user_path(@user)
     assert_template 'users/show'
+    # Title
     assert_select 'title', full_title(@user.name)
+    # Gravatar
     assert_select 'h1', text: @user.name
     assert_select 'h1>img.gravatar'
+    # Microposts with pagination
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination'
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+    # Following/follower stats
+    assert_select 'a[href=?]', following_user_path(@user)
+    assert_select 'a[href=?]', followers_user_path(@user)
+    assert_select '#following', text: @user.following.count.to_s
+    assert_select '#followers', text: @user.followers.count.to_s
   end
 end
