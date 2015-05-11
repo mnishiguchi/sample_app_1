c1ebe6ee26f42a92de4dea96bbdd789df666d213
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-05-11 11:51:32 -0400

    Add test for contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ApplicationMailer
 
   def message_from_user(contact)
-    @user = %Q["#{contact.name}" <#{contact.email}>]
     @message = contact.message
+    user_with_email = %Q("#{contact.name}" <#{contact.email}>)
 
     mail to: "masatoshi.nishiguchi@udc.edu",
          from: contact.email,
-         subject: "A message from #{@user}"
+         subject: "A message from #{user_with_email}"
   end
 end
--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -1,12 +1,21 @@
 require 'test_helper'
 
 class ContactMailerTest < ActionMailer::TestCase
-  # test "message_from_user" do
-  #   mail = ContactMailer.message_from_user
-  #   assert_equal "Message from user", mail.subject
-  #   assert_equal ["to@example.org"], mail.to
-  #   assert_equal ["from@example.com"], mail.from
-  #   assert_match "Hi", mail.body.encoded
-  # end
+
+  def setup
+    @contact = Contact.new(name:    "Example User",
+                           email:   "user@example.com",
+                           message: "Lorem ipsum dolor sit amet.")
+  end
+
+  test "message_from_user" do
+    mail = ContactMailer.message_from_user(@contact)
+    # Subject, from, to
+    assert_equal 'A message from "Example User" <user@example.com>', mail.subject
+    assert_equal ["masatoshi.nishiguchi@udc.edu"], mail.to
+    assert_equal [@contact.email], mail.from
+    # Contents
+    assert_match @contact.message, mail.body.encoded
+  end
 
 end
